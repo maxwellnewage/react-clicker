b94e3bd9aa36249f109282dc8dfeee6c99810aa4
Maximiliano Burgos <maxib2007@gmail.com>
2023-05-10 15:24:32 -0300

    se implemento compra y venta de tienda

--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ItemType } from "../../data/shop_items";
+import CookieContext from "../context/CookieContext";
 
 interface ShopItemProps {
   item: ItemType;
@@ -7,6 +8,15 @@ interface ShopItemProps {
 
 function ShopItem({ item }: ShopItemProps) {
   const itemSellingCost = item.cost / 2;
+  const {cookieAmount, setCookieAmount} = useContext(CookieContext)
+
+  const transaction = (action: 'B'|'S') => () => {
+    if (action == 'B') {
+      setCookieAmount(cookieAmount - item.cost)
+    } else {
+      setCookieAmount(cookieAmount + itemSellingCost)
+    }
+  }
 
   return (
     <div className="row m-2">
@@ -18,11 +28,16 @@ function ShopItem({ item }: ShopItemProps) {
       </div>
 
       <div className="col">
-        <button className="btn btn-success">
+        <button 
+          className="btn btn-success" 
+          disabled={cookieAmount < item.cost}
+          onClick={transaction('B')}>
           <h5>${item.cost}</h5>
         </button>
 
-        <button className="ms-3 btn btn-danger">
+        <button 
+          className="ms-3 btn btn-danger"
+          onClick={transaction('S')}>
           <h5>${itemSellingCost}</h5>
         </button>
       </div>
