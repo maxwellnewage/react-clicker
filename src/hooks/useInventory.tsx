e27db90c949e8bd3f8dedfd12e029e52223a54fd
Maximiliano Burgos <maxib2007@gmail.com>
2023-05-15 18:26:39 -0300

    se configuro proyecto para electron

--- src/hooks/useInventory.tsx
+++ src/hooks/useInventory.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 import { ItemType } from "../../data/shop_items";
 import InventoryContext, { InventoryType } from "../context/InventoryContext";
 
